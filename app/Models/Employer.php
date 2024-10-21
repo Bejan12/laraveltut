694e8125a0070a33c62f360ec6e683f037799fec
Bejan Afkar <336498@student.mboutrecht.nl>
2024-10-21 12:32:15 +0200

    filmpje 14

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Job extends Model
+class Employer extends Model
 {
     use HasFactory;
 
-    // Definieer de relatie met de Employer
-    public function employer()
+    public function jobs()
     {
-        return $this->belongsTo(Employer::class);
+        return $this->hasMany(Job::class);
     }
 }
